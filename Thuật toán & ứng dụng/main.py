8b9c4114bcbe7cda5b7efa047b570677cd97289f
tuanpham21105 <tuan.phamminh936@gmail.com>
2025-10-13 19:57:10 +0700

    update: TT-UD main

--- Thuật toán & ứng dụng/main.py
+++ Thuật toán & ứng dụng/main.py
@@ -1,26 +1,47 @@
-class ps:
-    def __init__(self, _t = 0, _m = 0):
-        self.t, self.m = _t, _m
-    def __add__(self, other):
-        x = ps(self, t * other.m + self.m * other.t, self.m * other.m)
-        x.rg()
-        return x
-    def __str__(self):
-        return str(self.t) + "/" + str(self.m)
-    def rg(self):
-        d = abs(math.gcd(self.t, self.m))
-        if self.m < 0:
-            d = -d
-        self.t //= d
-        self.m //= d
-    def nghichdao(self):
-        return ps(self.m, self.t)
+# class ps:
+#     def __init__(self, _t = 0, _m = 0):
+#         self.t, self.m = _t, _m
+#     def __add__(self, other):
+#         x = ps(self, t * other.m + self.m * other.t, self.m * other.m)
+#         x.rg()
+#         return x
+#     def __str__(self):
+#         return str(self.t) + "/" + str(self.m)
+#     def rg(self):
+#         d = abs(math.gcd(self.t, self.m))
+#         if self.m < 0:
+#             d = -d
+#         self.t //= d
+#         self.m //= d
+#     def nghichdao(self):
+#         return ps(self.m, self.t)
 
-if __name__ == '__main__':
-    n = int(input())
-    a = list(map(int, input().split()))[::1]
-    p = ps(0, 1)
-    for x in a[1:]:
-        q = p.nghichdao()
-        p = ps(x, 1) + q
-    print(p)
+# if __name__ == '__main__':
+#     n = int(input())
+#     a = list(map(int, input().split()))[::1]
+#     p = ps(0, 1)
+#     for x in a[1:]:
+#         q = p.nghichdao()
+#         p = ps(x, 1) + q
+#     print(p)
+
+###13/10/2025
+
+##Lap ghep ong nuoc
+from queue import PriorityQueue
+q = PriorityQueue()
+n, k = map(int, input().split())
+for i in map(int, input().split()):
+    q.put(i)
+
+sum = 0
+while q.qsize() > 1:
+    temp = 0
+    for i in range(k):
+        if (q.qsize() == 0):
+            break
+        temp += q.get()
+    q.put(temp)
+    sum += temp
+
+print(sum)
